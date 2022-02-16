1d4660a6b97a014f0a186b82aadf300e19a36f9d
= <anguyen031@gmail.com>
2022-02-15 19:30:03 -0800

    Style accordion

--- src/components/RenderCanines.js
+++ src/components/RenderCanines.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import { Row, Col, Card, CardBody, CardTitle, CardHeader, CardText, Accordion, AccordionItem, AccordionHeader, AccordionBody, UncontrolledCollapse, Collapse } from "reactstrap";
 
+
 const imagePath = "https://cdn2.thedogapi.com/images/";
 
 export default class RenderCanines extends Component {
   constructor(props) {
     super(props);
-    this.state = { synonymInfo: [], col: 0, cardItems: [1, 2, 3, 4]};
+    this.state = { synonymInfo: [], col: 0 }
     this.tempArray = [];
     this.canineImageID = this.props.canineInfo.reference_image_id;
     this.addErrorSrc = this.addErrorSrc.bind(this);
@@ -24,7 +25,7 @@ export default class RenderCanines extends Component {
   }
 
   render() {
-   return(
+    return(
     <Row className="d-flex g-0 text-center canine-card-row">
       <Col xs={{offset: 1, size: 10}}>
         <Card className="canine-card">
@@ -32,48 +33,44 @@ export default class RenderCanines extends Component {
             <Col xs={12}>
               <CardTitle><h2>{this.props.canineInfo.name}</h2></CardTitle>
             </Col>
-            <Col xs={12}>
+            <Col xs={12} className="mb-3">
               <div className="dog-image-holder">
                 <img className="w-100" onError={this.addErrorSrc}
                 src={`${imagePath}${this.canineImageID}.jpg`}
-                // TODO - make it so the pitbul has the .png extension
-                // TODO - allow random images, too
-                // The pit bull one is .png
                 alt={this.props.canineInfo.name}/>
               </div>
             </Col>
             <Col>
               <Card>
-                <CardHeader onClick={this.toggle} data-event={1}>Historical Job</CardHeader>
+                <button className="cardDropdownHeader w-100 btn btn-outline-primary" onClick={this.toggle} data-event={1}>Historical Job <i className={`fa fa-chevron-down ${this.state.col === 1 ? "rotate-chevron" : ""}`}></i></button>
                 <Collapse isOpen={this.state.col === 1}>
                 <CardBody>{this.props.canineInfo.bred_for}</CardBody>
                 </Collapse>
               </Card>
               <Card>
-                <CardHeader onClick={this.toggle} data-event={2}>Height and Weight</CardHeader>
+              <button className="cardDropdownHeader w-100 btn btn-outline-primary" onClick={this.toggle} data-event={2}>Height and Weight <i className={`fa fa-chevron-down ${this.state.col === 2 ? "rotate-chevron" : ""}`}></i></button>
                 <Collapse isOpen={this.state.col === 2}>
                 <CardBody>{this.props.canineInfo.height.imperial}
                 {this.props.canineInfo.weight.imperial}</CardBody>
                 </Collapse>
               </Card>
               <Card>
-                <CardHeader onClick={this.toggle} data-event={3}>Life Span</CardHeader>
+              <button className="cardDropdownHeader w-100 btn btn-outline-primary" onClick={this.toggle} data-event={3}>Life Span <i className={`fa fa-chevron-down ${this.state.col === 3 ? "rotate-chevron" : ""}`}></i></button>
                 <Collapse isOpen={this.state.col === 3}>
                 <CardBody>{this.props.canineInfo.life_span}</CardBody>
                 </Collapse>
               </Card>
               <Card>
-                <CardHeader onClick={this.toggle} data-event={4}>Temperament</CardHeader>
+              <button className="cardDropdownHeader w-100 btn btn-outline-primary" onClick={this.toggle} data-event={4}>Temperament <i className={`fa fa-chevron-down ${this.state.col === 4 ? "rotate-chevron" : ""}`}></i></button>
                 <Collapse isOpen={this.state.col === 4}>
                 <CardBody>{this.props.canineInfo.temperament}</CardBody>
                 </Collapse>
               </Card>
             </Col>
-  </CardBody>
-  </Card>
-  </Col>
-  </Row>
-    
+          </CardBody>
+        </Card>
+      </Col>
+    </Row>
     );
   }
 }
