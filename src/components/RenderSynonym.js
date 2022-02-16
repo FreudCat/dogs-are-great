ec8662aec92e1f4a12225c65416120b5e6e40c97
= <anguyen031@gmail.com>
2022-02-15 19:59:03 -0800

    Add modal

--- src/components/RenderSynonym.js
+++ src/components/RenderSynonym.js
@@ -1,27 +1,40 @@
 import React, { Component } from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Row, Col } from 'reactstrap';
 
 export default class RenderSynonym extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      modal: false
+    };
     this.synonymArray = this.props.synonymArray;
+    this.toggle = this.toggle.bind(this);
   }
-
-  // componentDidMount = (props) => {
-  //   const words = props.synonymInfo;
-  //   const listItems = words.map((word) => <li>{word}</li>);
-  //   console.log(listItems);
-  // };
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
 
   render() {
     console.log(this.synonymArray);
     return (
-      <div>
-        <h3>Synonymous Temperaments</h3>
-        {this.synonymArray.map((synonym) => (
-          
+      <Row className="g-0">
+        <Col xs={{offset: 2, size: 8}} className="mb-5">
+          <Button color="primary" onClick={this.toggle}>Get Synonymous Temperaments</Button>
+        </Col>
+        <Modal isOpen={this.state.modal} toggle={this.toggle}>
+          <ModalHeader toggle={this.toggle}>Synonyms</ModalHeader>
+          <ModalBody>
+          {this.synonymArray.map((synonym) => (
           <p>{synonym[0].word}</p>
         ))}
-      </div>
+          </ModalBody>
+          <ModalFooter>
+            <p>Pretty great dog!</p>
+          </ModalFooter>
+        </Modal>
+    </Row>
     );
   }
 }
